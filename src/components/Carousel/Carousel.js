da06dccf71878374b2c568140bf7cbbe5b3edc20
Dillon Dugan <dugan_dillon@yahoo.com>
2020-10-11 22:40:36 -0700

    webp optimizations for images

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -12,36 +12,36 @@ import { useMediaQuery } from "@material-ui/core";
 import theme from "../../styles/theme";
 import FontAwesomeIcon from "react-fontawesome";
 import useStyles from "./styles";
-import BD1 from "./BabyDillon1.png";
-import BD2 from "./BabyDillon2.png";
-import BD3 from "./BabyDillon3.png";
-import BD4 from "./BabyDillon4.png";
-import BD5 from "./BabyDillon5.png";
-import BD6 from "./BabyDillon6.png";
-import BD7 from "./BabyDillon7.png";
-import BD8 from "./BabyDillon8.png";
-import BD9 from "./BabyDillon9.png";
-import BD10 from "./BabyDillon10.png";
-import BD11 from "./BabyDillon11.png";
-import BD12 from "./BabyDillon12.png";
-import BD13 from "./BabyDillon13.png";
-import BD14 from "./BabyDillon14.png";
-import BD15 from "./BabyDillon15.png";
-import BT1 from "./BabyTina1.png";
-import BT2 from "./BabyTina2.png";
-import BT3 from "./BabyTina3.png";
-import BT4 from "./BabyTina4.png";
-import BT5 from "./BabyTina5.png";
-import BT6 from "./BabyTina6.png";
-import BT7 from "./BabyTina7.png";
-import BT8 from "./BabyTina8.png";
-import BT9 from "./BabyTina9.png";
-import BT10 from "./BabyTina10.png";
-import BT11 from "./BabyTina11.png";
-import BT12 from "./BabyTina12.png";
-import BT13 from "./BabyTina13.png";
-import BT14 from "./BabyTina14.png";
-import BT15 from "./BabyTina15.png";
+import BD1 from "./BabyDillon1.webp";
+import BD2 from "./BabyDillon2.webp";
+import BD3 from "./BabyDillon3.webp";
+import BD4 from "./BabyDillon4.webp";
+import BD5 from "./BabyDillon5.webp";
+import BD6 from "./BabyDillon6.webp";
+import BD7 from "./BabyDillon7.webp";
+import BD8 from "./BabyDillon8.webp";
+import BD9 from "./BabyDillon9.webp";
+import BD10 from "./BabyDillon10.webp";
+import BD11 from "./BabyDillon11.webp";
+import BD12 from "./BabyDillon12.webp";
+import BD13 from "./BabyDillon13.webp";
+import BD14 from "./BabyDillon14.webp";
+import BD15 from "./BabyDillon15.webp";
+import BT1 from "./BabyTina1.webp";
+import BT2 from "./BabyTina2.webp";
+import BT3 from "./BabyTina3.webp";
+import BT4 from "./BabyTina4.webp";
+import BT5 from "./BabyTina5.webp";
+import BT6 from "./BabyTina6.webp";
+import BT7 from "./BabyTina7.webp";
+import BT8 from "./BabyTina8.webp";
+import BT9 from "./BabyTina9.webp";
+import BT10 from "./BabyTina10.webp";
+import BT11 from "./BabyTina11.webp";
+import BT12 from "./BabyTina12.webp";
+import BT13 from "./BabyTina13.webp";
+import BT14 from "./BabyTina14.webp";
+import BT15 from "./BabyTina15.webp";
 
 export default function Carousel() {
   const classes = useStyles();
@@ -52,92 +52,92 @@ export default function Carousel() {
       <>
         <Slide index={0}>
           <Image src={BD1}></Image>
-        </Slide>{" "}
-        <Slide index={0}>
+        </Slide>
+        <Slide index={1}>
           <Image src={BT1}></Image>
         </Slide>
-        <Slide index={0}>
+        <Slide index={2}>
           <Image src={BD2}></Image>
-        </Slide>{" "}
-        <Slide index={0}>
+        </Slide>
+        <Slide index={3}>
           <Image src={BT2}></Image>
         </Slide>
-        <Slide index={0}>
+        <Slide index={4}>
           <Image src={BD3}></Image>
         </Slide>
-        <Slide index={0}>
+        <Slide index={5}>
           <Image src={BT3}></Image>
         </Slide>
-        <Slide index={0}>
+        <Slide index={6}>
           <Image src={BD4}></Image>
         </Slide>
-        <Slide index={0}>
+        <Slide index={7}>
           <Image src={BT4}></Image>
         </Slide>
-        <Slide index={0}>
+        <Slide index={8}>
           <Image src={BD5}></Image>
         </Slide>
-        <Slide index={0}>
+        <Slide index={9}>
           <Image src={BT5}></Image>
         </Slide>
-        <Slide index={0}>
+        <Slide index={10}>
           <Image src={BD6}></Image>
         </Slide>
-        <Slide index={0}>
+        <Slide index={11}>
           <Image src={BT6}></Image>
         </Slide>
-        <Slide index={0}>
+        <Slide index={12}>
           <Image src={BD7}></Image>
         </Slide>
-        <Slide index={0}>
+        <Slide index={13}>
           <Image src={BT7}></Image>
         </Slide>
-        <Slide index={0}>
+        <Slide index={14}>
           <Image src={BD8}></Image>
         </Slide>
-        <Slide index={0}>
+        <Slide index={15}>
           <Image src={BT8}></Image>
         </Slide>
-        <Slide index={0}>
+        <Slide index={16}>
           <Image src={BD9}></Image>
         </Slide>
-        <Slide index={0}>
+        <Slide index={17}>
           <Image src={BT9}></Image>
         </Slide>
-        <Slide index={0}>
+        <Slide index={18}>
           <Image src={BD10}></Image>
         </Slide>
-        <Slide index={0}>
+        <Slide index={19}>
           <Image src={BT10}></Image>
         </Slide>
-        <Slide index={0}>
+        <Slide index={20}>
           <Image src={BD11}></Image>
         </Slide>
-        <Slide index={0}>
+        <Slide index={21}>
           <Image src={BT11}></Image>
         </Slide>
-        <Slide index={0}>
+        <Slide index={22}>
           <Image src={BD12}></Image>
         </Slide>
-        <Slide index={0}>
+        <Slide index={23}>
           <Image src={BT12}></Image>
         </Slide>
-        <Slide index={0}>
+        <Slide index={24}>
           <Image src={BD13}></Image>
         </Slide>
-        <Slide index={0}>
+        <Slide index={25}>
           <Image src={BT13}></Image>
         </Slide>
-        <Slide index={0}>
+        <Slide index={26}>
           <Image src={BD14}></Image>
         </Slide>
-        <Slide index={0}>
+        <Slide index={27}>
           <Image src={BT14}></Image>
         </Slide>
-        <Slide index={0}>
+        <Slide index={28}>
           <Image src={BD15}></Image>
-        </Slide>{" "}
-        <Slide index={0}>
+        </Slide>
+        <Slide index={29}>
           <Image src={BT15}></Image>
         </Slide>
       </>
